069c4300a5003f86f8b7e88cc57a4699858ae0c1
vcher <chernyaev_v@media-effect.ru>
2026-09-24 09:44:09 +0300

    add PATH for haskell

--- lisp/lang/my-haskell.el
+++ lisp/lang/my-haskell.el
@@ -2,4 +2,8 @@
   :ensure t
   :hook (haskell-mode . eglot-ensure))
 
+(let ((dir (expand-file-name "~/.ghcup/bin")))
+  (add-to-list 'exec-path dir)
+  (setenv "PATH" (concat dir path-separator (getenv "PATH"))))
+
 (provide 'lang/my-haskell)
