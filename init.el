8c7fffd2da862782ce5df4a366bf1f2593958b18
vcher <chernyaev_v@media-effect.ru>
2026-09-24 09:36:21 +0300

    add custom.el file for custom-set-variables section

--- init.el
+++ init.el
@@ -12,24 +12,5 @@
 (require 'my-vc)
 (require 'my-prog)
 (require 'my-workspace)
-
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(custom-safe-themes
-   '("e0fbe5caa6e602975e59cdd381c9773a670a864dd7bb7bf2345414856148098b"
-     default))
- '(package-selected-packages nil)
- '(package-vc-selected-packages
-   '((treesit-fold :url
-                   "https://github.com/emacs-tree-sitter/treesit-fold")))
- '(warning-suppress-log-types '((treesit))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
-(put 'narrow-to-region 'disabled nil)
+(setq custom-file (expand-file-name "custom.el" user-emacs-directory))
+(load custom-file 'noerror 'nomessage)
